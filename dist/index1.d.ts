2a4ad7ddb2f5db4f17644cc1231e7c90fa684450
Noémien Kocher <nkcr.je@gmail.com>
2020-02-26 11:12:56 +0100

    Cleans up the dist folder

--- dist/index1.d.ts
+++
@@ -1,4 +0,0 @@
-import { SkipchainRPC, SkipBlock } from '@dedis/cothority/skipchain';
-export declare function sayHi(): void;
-export declare function printBlock(r: SkipBlock, rpc: SkipchainRPC): void;
-export declare function prependLog(...nodes: Array<Node | any>): void;
--- dist/index3.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function sayHi(): void;
-export declare function prependLog(...nodes: Array<Node | any>): void;
